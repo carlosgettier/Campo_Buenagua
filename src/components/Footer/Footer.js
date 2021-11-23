c570b9654ca114f00b5bccc1fc3ff5bc85e7e828
carlosgettier <carlosgettier27@gmail.com>
2021-11-23 19:31:20 -0300

    terminado

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => {
         <div className="footer">
             <Link to="video"  ><p className="footer-a">BUENAGUA</p></Link>
             <a href="https://www.instagram.com/buenaguachile/" target="_blank"><i class="fab fa-instagram"> @buenaguachile</i></a>
-            <p><i class="fas fa-mobile-alt"></i>  +5491154816748</p>
+            <p><i class="fas fa-mobile-alt"></i>  <i class="fab fa-whatsapp-square"></i>  +5491154816748</p>
         </div>
     )
 }
